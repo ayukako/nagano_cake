87be7f0d4da13d2a30d106eda3e6e4aaa9e622fb
ayukako <yukako1102n@gmail.com>
2022-04-23 07:57:53 +0000

    fixed controller regstraitions,sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   namespace :admin do
     get 'orders_details/update'
   end
@@ -80,16 +93,5 @@ Rails.application.routes.draw do
     get 'homes/top'
     get 'homes/about'
   end
-# 顧客用
-# URL /customers/sign_in ...
-devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
 
-# 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
 end
