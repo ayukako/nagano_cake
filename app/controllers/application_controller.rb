c3b9ff701e738f5e2de2bea6a90dc6794f0cf1a5
ayukako <yukako1102n@gmail.com>
2022-04-24 09:55:38 +0000

    add app/controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
+
+def after_sign_in_path_for(resource)
+  case resource
+  when Admin
+    admin_root_path
+  when Customer
+   root_path
+  end
+end
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -12,41 +12,42 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
-  # namespace :admin do
-  #   get 'orders_details/update'
-  # end
-  # namespace :admin do
-  #   get 'orders/show'
-  #   get 'orders/update'
-  # end
-  # namespace :admin do
-  #   get 'customers/index'
-  #   get 'customers/show'
-  #   get 'customers/edit'
-  #   get 'customers/update'
-  # end
-  # namespace :admin do
-  #   get 'genres/index'
-  #   get 'genres/create'
-  #   get 'genres/edit'
-  #   get 'genres/update'
-  # end
-  # namespace :admin do
-  #   get 'items/index'
-  #   get 'items/new'
-  #   get 'items/create'
-  #   get 'items/show'
-  #   get 'items/edit'
-  #   get 'items/update'
-  # end
-  # namespace :admin do
-  #   get 'homes/top'
-  # end
-  # namespace :admin do
-  #   get 'sessions/new'
-  #   get 'sessions/create'
-  #   get 'sessions/destroy'
-  # end
+  namespace :admin do
+    get 'orders_details/update'
+  end
+  namespace :admin do
+    get 'orders/show'
+    get 'orders/update'
+  end
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+    get 'customers/update'
+  end
+  namespace :admin do
+    get 'genres/index'
+    get 'genres/create'
+    get 'genres/edit'
+    get 'genres/update'
+  end
+  namespace :admin do
+    get 'items/index'
+    get 'items/new'
+    get 'items/create'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  namespace :admin do
+    get 'homes/top'
+    root to: "homes#top"
+  end
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+    get 'sessions/destroy'
+  end
   namespace :public do
     get 'addresses/index'
     get 'addresses/edit'
