f22fe8c924f157dc72fc2803d01d423f19d80617
ayukako <yukako1102n@gmail.com>
2022-04-27 11:42:24 +0000

    made genre index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,24 @@
 class ApplicationController < ActionController::Base
 
 
-def after_sign_in_path_for(resource)
-  case resource
-  when Admin
-    admin_root_path
-  when Customer
-   root_path
-  end
-end
+# def after_sign_in_path_for(resource)
+#   case resource
+#   when Admin
+#     admin_root_path
+#   when Customer
+#   root_path
+#   end
+# end
 
 
-def after_sign_out_path_for(resource)
-  case resource
-  when Admin
-    new_admin_session_path
-  when Customer
-   public_items_index_path
-  end
-  protected_methods
-end
+# def after_sign_out_path_for(resource)
+#   case resource
+#   when Admin
+#     new_admin_session_path
+#   when Customer
+#   public_items_index_path
+#   end
 
+# end
 end
 
