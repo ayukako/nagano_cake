d842179013ad1bd38bf29686d0bf9985cab8aede
ayukako <yukako1102n@gmail.com>
2022-04-30 13:10:28 +0000

    made address_edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -18,7 +18,7 @@ class Public::AddressesController < ApplicationController
   def update
     address = Address.find(params[:id])
     address.update(address_params)
-    redirect_to public_address_path(address.id)
+    redirect_to public_addresses_path(address.id)
   end
 
   def destroy
