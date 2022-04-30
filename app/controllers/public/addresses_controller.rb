5e81d39906544a97753ae3a61c587d74203dc03b
ayukako <yukako1102n@gmail.com>
2022-04-30 12:32:46 +0000

    made address_index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,34 @@
 class Public::AddressesController < ApplicationController
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to public_address_path(address.id)
   end
 
   def destroy
   end
+
+
+  private
+
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
