912ea544d8878bdb84f15a31280229af4c03760c
ayukako <yukako1102n@gmail.com>
2022-05-07 06:30:41 +0000

    made order(new,confirm,thanks)

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,15 +1,27 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+
   end
 
   def confirm
+    @cart_items = current_customer.cart_items
     @order = Order.new(order_params)
+    @order.postage = 800
     if params[:select_address] == 1
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.name
     elsif params[:select_address] == 2
+      @address = Address.find(params[:address_id])
+      @order.postal_code = @address.postal_code
+      @order.address = @address.address
+      @order.name = @address.name
     else params[:select_address] == 3
+      @order.postal_code = params[:postal_code]
+      @order.address = params[:address]
+      @order.name = params[:name]
     end
-    redirect_to confirm_public_orders_path
   end
 
   def thanks
@@ -17,26 +29,17 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
-    if params[:select_address] == 1
-      customer.address.save
-      #自身の住所を保存する処理
-    elsif params[:select_address] == 2
-      customer.address.save
-      #登録済みの住所を保存する処理
-    else params[:select_address] == 3
-    end
-
+    @order.save
   end
 
   def index
   end
 
   def show
-    @order = Order.find(params[:id])
   end
 
   def order_params
-  params.require(:order).permit(:payment_method, :postal_code, :address, :name, :order_status)
+  params.require(:order).permit(:payment_method, :postal_code, :address, :name, :order_status, :postage)
   end
 
 end
