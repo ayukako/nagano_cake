50488a5dc0b07e67403fd5c1e5d8fbe3ab45b5a8
ayukako <yukako1102n@gmail.com>
2022-05-10 13:26:02 +0000

    making public/order_details,fixed Admin/item edit

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,5 +8,5 @@ class Order < ApplicationRecord
   end
 
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum order_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4 }
+  enum order_status: { waiting:0, confirmation:1, making:2, packing:3, sended:4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -4,6 +4,6 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
 
 
-  enum production_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済":4 }
+  enum production_status: { waiting:0, confirmation:1, making:2, packing:3, sended:4 }
 
 end
